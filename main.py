3c5f4a905ad59cba5f041baededb299f4d6dc9f7
MrAnjelon <maximbukhvalov.ru@yandex.ru>
2023-02-16 18:38:50 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+eng_alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+massage = input().lower()
+new_word = ''
+step = int(input())
+
+for i in massage:
+    find_place = eng_alphabet.find(i) #вычисляем места стимволов
+    new_place = find_place + step #сдвигаем символ на указанный шаг
+    if i in eng_alphabet:
+        new_word +=eng_alphabet[new_place]
+    else:
+        new_word +=i
+        
+print(new_word)
